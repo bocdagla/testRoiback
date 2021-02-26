a17696c7dcf70ff12350fe7c515fdbc84fc28dd3
Bartomeu Moranta <tomeu1m1a1@gmail.com>
2021-02-26 20:31:17 +0100

    added first module

---
+++ Frontend.test.src/receptionPage/src/app/singup/singup.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatRadioModule } from '@angular/material/radio'
+@Component({
+  selector: 'app-singup',
+  templateUrl: './singup.component.html',
+  styleUrls: ['./singup.component.scss']
+})
+
+export class SingupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
